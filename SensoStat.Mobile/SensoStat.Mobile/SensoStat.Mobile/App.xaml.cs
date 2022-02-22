0eb73de3fce672e7a5ca56460cf66a1baec3cfc1
Tristan DEVOILLE <tristan.devoille@diiage.org>
2022-02-22 10:10:28 +0100

    feat : [CreateSolution] Création de la solution Xamarin

---
+++ SensoStat.Mobile/SensoStat.Mobile/SensoStat.Mobile/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using SensoStat.Mobile.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SensoStat.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
