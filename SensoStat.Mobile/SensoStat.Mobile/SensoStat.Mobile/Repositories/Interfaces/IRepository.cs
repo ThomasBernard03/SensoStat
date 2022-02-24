64395a7296394fe21bdd19161608b36d47c805f6
ThomasBernardPageUp <tbernard@pageup.fr>
2022-02-24 13:05:42 +0100

    add repo + http service

---
+++ SensoStat.Mobile/SensoStat.Mobile/SensoStat.Mobile/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SensoStat.Mobile.Repositories.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Clear();
+        void Delete(T value);
+        T Insert(T value);
+        T Update(T value);
+        T GetById(int id);
+        List<T> Get();
+    }
+}
+
