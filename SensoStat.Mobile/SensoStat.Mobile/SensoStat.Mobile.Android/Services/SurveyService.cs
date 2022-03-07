f3211618f86703c1b1c79c30e0d90be54e275e9d
Baptiste RAMEAU <baptiste.rameau@diiage.org>
2022-03-07 11:01:35 +0100

    feat: [SurveyService] create GetSurvey() method with Call Http to API in Android and iOS

---
+++ SensoStat.Mobile/SensoStat.Mobile/SensoStat.Mobile.Android/Services/SurveyService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SensoStat.Mobile.Commons;
+using SensoStat.Mobile.Models;
+using SensoStat.Mobile.Services.Interfaces;
+
+namespace SensoStat.Mobile.Droid.Services
+{
+	public class SurveyService
+	{
+        private readonly IHttpService _httpService;
+
+        public SurveyService(IHttpService httpService)
+        {
+            _httpService = httpService;
+        }
+
+        public async Task<Survey> GetSurvey(int surveyId, string token = "")
+        {
+            var survey = await _httpService.SendHttpRequest<Survey>($"{Constants.BaseUrlApi}survey?surveyId={surveyId}", HttpMethod.Get, bearer: token);
+
+            return survey;
+        }
+    }
+}
+
