9ce806f1ce5e8432ac2caec2640621137c73d2f6
Thomas BERNARD <tbernard@pageup.fr>
2022-03-15 14:46:19 +0100

    allow user to enter an url

--- SensoStat.Mobile/SensoStat.Mobile/SensoStat.Mobile/ViewModels/StartViewModel.cs
+++ SensoStat.Mobile/SensoStat.Mobile/SensoStat.Mobile/ViewModels/StartViewModel.cs
@@ -15,6 +15,7 @@ namespace SensoStat.Mobile.ViewModels
         public StartViewModel(INavigationService navigationService, ISpeechService speechService) : base(navigationService)
         {
             StartSurveyCommand = new DelegateCommand(async () => await OnStartSurvey());
+            CheckUserLinkCommand = new DelegateCommand(async () => await OnCheckUserLink());
 
             _speechService = speechService;
         }
@@ -30,19 +31,44 @@ namespace SensoStat.Mobile.ViewModels
 
             _speechService.SpeechRecognizer.Recognized += async (object sender, Microsoft.CognitiveServices.Speech.SpeechRecognitionEventArgs e) =>
             {
-                if (e.Result.Text.ToLower().Contains("commencer"))
+                if (e.Result.Text.ToLower().Contains("commencer") && IsLinkValid)
                 {
                     await OnStartSurvey();
                 }
             };
         }
 
+        #region StartSurveyCommand => OnStartSurvey
         public DelegateCommand StartSurveyCommand { get; set; }
         private async Task OnStartSurvey()
         {
             await _speechService.StopTextToSpeech();
             //await _speechService.SpeechRecognizer?.StopContinuousRecognitionAsync();
             MainThread.BeginInvokeOnMainThread(async () => { var temp = await NavigationService.NavigateAsync(Commons.Constants.InstructionPage); });
+        }
+        #endregion
+
+        #region CheckUserLinkCommand => OnCheckUserLink
+        public DelegateCommand CheckUserLinkCommand { get; set; }
+        private async Task OnCheckUserLink()
+        {
+
+        }
+        #endregion
+
+        private string _userLink;
+        public string UserLink
+        {
+            get { return _userLink; }
+            set { SetProperty(ref _userLink, value); }
+        }
+
+
+        private bool _isLinkValid;
+        public bool IsLinkValid
+        {
+            get { return _isLinkValid; }
+            set { SetProperty(ref _isLinkValid, value); }
         }
     }
 }
